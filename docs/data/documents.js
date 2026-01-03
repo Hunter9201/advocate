4c0e4f9e52e9f495a06a20fd9c9f56b0bade5695
hunter9201.github.io <LionHunter920@gmail.com>
2026-01-03 16:39:25 +0300

    Update legal documents database

---
+++ docs/data/documents.js
@@ -0,0 +1,74 @@
+const ugandaLegalDocuments = [
+  {
+    "id": 1,
+    "title": "Uganda Citizenship And Immigration Control Act Cap 313",
+    "description": "An Act relating to uganda citizenship and immigration control  cap 313",
+    "filename": "Uganda Citizenship and Immigration Control Act Cap 313.pdf",
+    "category": "constitutional",
+    "size": "1.5 MB",
+    "pages": 50,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2026"
+  },
+  {
+    "id": 2,
+    "title": "Uganda Citizenship And Immigration Control Regulations",
+    "description": "Regulations for uganda citizenship and immigration control",
+    "filename": "Uganda Citizenship and Immigration Control Regulations 2004.pdf",
+    "category": "procedure",
+    "size": "0.5 MB",
+    "pages": 17,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2004"
+  },
+  {
+    "id": 3,
+    "title": "Uganda Communications Act Cap 103.",
+    "description": "An Act relating to uganda communications  cap 103.",
+    "filename": "Uganda Communications Act Cap 103..pdf",
+    "category": "constitutional",
+    "size": "1.5 MB",
+    "pages": 50,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2026"
+  },
+  {
+    "id": 4,
+    "title": "The Trade(licensing) Act Cap 79",
+    "description": "An Act relating to the trade(licensing)  cap 79",
+    "filename": "The Trade(Licensing) Act Cap 79.pdf",
+    "category": "constitutional",
+    "size": "1.0 MB",
+    "pages": 33,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2026"
+  },
+  {
+    "id": 5,
+    "title": "Trade Secrets Protection Act Cap 80",
+    "description": "An Act relating to trade secrets protection  cap 80",
+    "filename": "Trade Secrets Protection Act Cap 80.pdf",
+    "category": "constitutional",
+    "size": "0.3 MB",
+    "pages": 10,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2026"
+  },
+  {
+    "id": 6,
+    "title": "Trademarks Regulations",
+    "description": "Regulations for trademarks",
+    "filename": "Trademarks Regulations 2023.pdf",
+    "category": "procedure",
+    "size": "1.2 MB",
+    "pages": 40,
+    "date": "2026-01-03",
+    "updated": false,
+    "year": "2023"
+  }
+];
